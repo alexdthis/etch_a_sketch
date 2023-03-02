b4f950ba5b4856c7fc93a8908c21fcc5ee6fe1b3
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-01 21:43:59 -0800

    Rewrite createPadd function
    Rewrote and added if else statements in order classify
    boxes as top, bottom, left, and right boxes

--- hoverboard.js
+++ hoverboard.js
@@ -6,7 +6,7 @@ function createPad (number) {
     for (let length = 1; length <= lengthOfPad; length++) {
         for (let width = 1; width <= lengthOfPad; width++) {
             let x_coord = document.createElement("div");
-            /* Bunch of if else statements to check if a box is a corner box */
+            /* Bunch of if else statements to check if a box is a corner box or a border box */
             if (width === 1 && length === 1) {
                 x_coord.classList.add("topLeftBox");
                 container.appendChild(x_coord);
@@ -19,16 +19,18 @@ function createPad (number) {
             } else if (width === lengthOfPad && length === lengthOfPad) {
                 x_coord.classList.add("bottomRightBox");
                 container.appendChild(x_coord);
-                
-            /* if statement checks if operands lie on the outside and
-                assigns border box class to them
-                If boxes are on corners, it skips over them */
-            } else if ((length === 1) 
-                || (width === 1)
-                || (length === lengthOfPad) 
-                || (width === lengthOfPad)) {
-                    x_coord.classList.add("borderBox");
-                    container.appendChild(x_coord);
+            } else if (length === 1) {
+                x_coord.classList.add("topBoxes");
+                container.appendChild(x_coord);
+            } else if (length === lengthOfPad) {
+                x_coord.classList.add("bottomBoxes");
+                container.appendChild(x_coord);
+            } else if (width === 1) {
+                x_coord.classList.add("leftBoxes");
+                container.appendChild(x_coord);
+            } else if (width === lengthOfPad) {
+                x_coord.classList.add("rightBoxes");
+                container.appendChild(x_coord);
             } else {
             /* else statement assigns class individualBox to every other box */
                 x_coord.classList.add("individualBox");
