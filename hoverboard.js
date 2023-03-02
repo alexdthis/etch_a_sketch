469cd80cb04669bf7fb86d7e0c83693b977f1638
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-01 22:23:03 -0800

    Remove individual box classes
    Added style classes for all boxes. After some thought, all if statements
    have been commented out and replaced with a single box class. The css file
    has been filled with basic styling and flex formatting to make a square grid.

--- hoverboard.js
+++ hoverboard.js
@@ -4,38 +4,44 @@ let container = document.querySelector(".container");
 function createPad (number) {
     let lengthOfPad = number;
     for (let length = 1; length <= lengthOfPad; length++) {
+        let row = document.createElement("p");
+        row.classList.add("rowContainer");
         for (let width = 1; width <= lengthOfPad; width++) {
             let x_coord = document.createElement("div");
-            /* Bunch of if else statements to check if a box is a corner box or a border box */
+            x_coord.classList.add("boxes");
+            row.appendChild(x_coord);
+
+            /* Bunch of if else statements to check if a box is a corner box or a border box 
             if (width === 1 && length === 1) {
                 x_coord.classList.add("topLeftBox");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else if (width === lengthOfPad && length === 1) {
                 x_coord.classList.add("topRightBox");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else if (width === 1 && length === lengthOfPad) {
                 x_coord.classList.add("bottomLeftBox");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else if (width === lengthOfPad && length === lengthOfPad) {
                 x_coord.classList.add("bottomRightBox");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else if (length === 1) {
                 x_coord.classList.add("topBoxes");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else if (length === lengthOfPad) {
                 x_coord.classList.add("bottomBoxes");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else if (width === 1) {
                 x_coord.classList.add("leftBoxes");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else if (width === lengthOfPad) {
                 x_coord.classList.add("rightBoxes");
-                container.appendChild(x_coord);
+                row.appendChild(x_coord);
             } else {
-            /* else statement assigns class individualBox to every other box */
+            else statement assigns class individualBox to every other box
                 x_coord.classList.add("individualBox");
-                container.appendChild(x_coord);
-            }
+                row.appendChild(x_coord); 
+            } */
         }
+        container.appendChild(row);
     }
 }
