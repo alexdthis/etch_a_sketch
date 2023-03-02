c33387d34c79dccf3217f4dfb3b25e8e248729e0
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-01 21:03:27 -0800

    Add new class
    Added new class "cornerBox" for boxes that are on each corner.
    Added more conditions to check for in the createPad function in
    order to assign this class.

--- hoverboard.js
+++ hoverboard.js
@@ -6,11 +6,31 @@ function createPad (number) {
     for (let length = 1; length <= lengthOfPad; length++) {
         for (let width = 1; width <= lengthOfPad; width++) {
             let x_coord = document.createElement("div");
-            if ((length === 1) || (width === 1)
-            || (length === lengthOfPad) || (width === lengthOfPad)) {
-                x_coord.classList.add("borderBox");
-                container.appendChild(x_coord);
-            } else {
+            /* 1st if statement checks if sum of x and y are equal to
+            2, 1 + length, or 2 * length, then assigns the following
+            divs class "cornerBox". If either x or y coordinate do not
+            equal 1 or the given length, it is skipped over */
+            if ((((length + width) === 2)
+                || ((length + width) === (1 + lengthOfPad))
+                || ((length + width) === (2 * lengthOfPad)))
+                && ((length === 1) || (width === 1)
+                || (length === lengthOfPad) || (width === lengthOfPad)))
+                    {
+                    x_coord.classList.add("cornerBox");
+                    container.appendChild(x_coord);
+            /* if statement checks if operands lie on the outside and
+                assigns border box class to them
+                If boxes are on corners, it skips over them */
+            } else if ((length === 1) 
+                || (width === 1)
+                || (length === lengthOfPad) 
+                || (width === lengthOfPad)) 
+                {
+                    x_coord.classList.add("borderBox");
+                    container.appendChild(x_coord);
+            } else 
+                {
+            /* else statement assigns class individualBox to every other box */
                 x_coord.classList.add("individualBox");
                 container.appendChild(x_coord);
             }
