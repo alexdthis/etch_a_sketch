ca3938f95278b56d76d2ce9ca0e8b1e4db6d18d3
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-01 21:26:17 -0800

    Added individual corner box classes
    Added individual corner box classes: topLeft, topRight,
    bottomLeft, bottomRight. Rewrote if statements for createPad
    to properly assign these classes.

--- hoverboard.js
+++ hoverboard.js
@@ -6,30 +6,30 @@ function createPad (number) {
     for (let length = 1; length <= lengthOfPad; length++) {
         for (let width = 1; width <= lengthOfPad; width++) {
             let x_coord = document.createElement("div");
-            /* 1st if statement checks if sum of x and y are equal to
-            2, 1 + length, or 2 * length, then assigns the following
-            divs class "cornerBox". If either x or y coordinate do not
-            equal 1 or the given length, it is skipped over */
-            if ((((length + width) === 2)
-                || ((length + width) === (1 + lengthOfPad))
-                || ((length + width) === (2 * lengthOfPad)))
-                && ((length === 1) || (width === 1)
-                || (length === lengthOfPad) || (width === lengthOfPad)))
-                    {
-                    x_coord.classList.add("cornerBox");
-                    container.appendChild(x_coord);
+            /* Bunch of if else statements to check if a box is a corner box */
+            if (width === 1 && length === 1) {
+                x_coord.classList.add("topLeftBox");
+                container.appendChild(x_coord);
+            } else if (width === lengthOfPad && length === 1) {
+                x_coord.classList.add("topRightBox");
+                container.appendChild(x_coord);
+            } else if (width === 1 && length === lengthOfPad) {
+                x_coord.classList.add("bottomLeftBox");
+                container.appendChild(x_coord);
+            } else if (width === lengthOfPad && length === lengthOfPad) {
+                x_coord.classList.add("bottomRightBox");
+                container.appendChild(x_coord);
+                
             /* if statement checks if operands lie on the outside and
                 assigns border box class to them
                 If boxes are on corners, it skips over them */
             } else if ((length === 1) 
                 || (width === 1)
                 || (length === lengthOfPad) 
-                || (width === lengthOfPad)) 
-                {
+                || (width === lengthOfPad)) {
                     x_coord.classList.add("borderBox");
                     container.appendChild(x_coord);
-            } else 
-                {
+            } else {
             /* else statement assigns class individualBox to every other box */
                 x_coord.classList.add("individualBox");
                 container.appendChild(x_coord);
