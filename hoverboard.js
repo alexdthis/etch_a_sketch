dcef21c07c54a4747d67b08c773926e1c1b7b834
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-02 20:13:17 -0800

    Add a color change function
    Added function changeColor along with an eventlistener for the new
    color fields at the bottom. Pressing submit will set colorDefault to
    0 and function colorHoverOver instead of hoverOver will be used instead.

--- hoverboard.js
+++ hoverboard.js
@@ -1,34 +1,100 @@
 
 let container = document.querySelector(".container");
 let submitForm = document.querySelector(".submitForLength");
+let submitValueTextField = document.querySelector("#lengthOfGrid");
+let colorSubmitButton = document.querySelector(".colorSubmitButton");
+let rValue = 0;
+let gValue = 0;
+let bValue = 0;
+
 let submittedValue = 0;
-    
+let defaultColor = 1;
+
+/* Test function to get keycodes
+
+window.addEventListener('keydown', (e) => {console.log(e)}); */
+/* This statement makes the submit button draw the square when clicked */
 submitForm.addEventListener('click', drawPad);
-container.addEventListener('mousemove', hoverOver);
+/* This one makes the text field draw when the square when enter is pressed */
+submitValueTextField.addEventListener("keydown", (e) => {
+    /* console.log(e); */
+    if (e.keyCode === 13) {
+        /* console.log(e); */
+        drawPad();
+    } else {
+        return;
+    }
+});
+
+/* This eventListener is linked to the submit button for the color */
+colorSubmitButton.addEventListener('click', changeColor);
+/* This eventListener is linked to the drawing function */
+container.addEventListener('mousemove', (e) => {
+    if (defaultColor === 0) {
+        colorHoverOver(e);
+    } else if (defaultColor === 1) {
+        hoverOver(e);
+
+    }
+});
+
+/* Fired when the grid gets made.
+playing class only gets added when the mouse
+is pressed down */
 
 function hoverOver(e) {
     if (e.target.classList[0] !== "boxes") {
         return;
     }
-
     if (e.buttons != 1) {
         return;
     }
-    console.log(e.target.classList);
+    /* console.log(e.target.classList); */
     e.target.classList.add("playing");
 }
-function drawPad(e) {
+
+function colorHoverOver (e) {
+    if (e.target.classList[0] !== "boxes") {
+        return;
+    }
+    if (e.buttons != 1) {
+        return;
+    }
+    e.target.style.backgroundColor = ("rgb(" + rValue + "," + gValue + "," + bValue);
+    /* Attempt at trying to assign a new class that contains a new color when 
+    a different color is selected. 
+    e.target.classList.add("newColored");
+    newColor = document.querySelectorAll(".newColored");
+    newColor.; */
+    /* e.target.backgroundColor = "red"; *//* String(`rgb($(rValue), $(gValue), $(bValue))`) */;
+    /* e.target.querySelector(".colored").style.backgroundColor = String(`rgb($(rValue), $(gValue), $(bValue))`); */
+}
+/* General function that is called when
+the submit button is clicked.
+Removes any old grid squares and redraws a new grid
+with a specified number */
+function changeColorLoop() {
+
+}
+
+function drawPad() {
     if(container.hasChildNodes()) {
         removeOldSquares();
     }
     createPad();
 }
 
+/*Removes an old grid */
+
 function removeOldSquares() {
     while(container.hasChildNodes()) {
         container.removeChild(container.childNodes[0]);
     }
 }
+
+/* Function that makes x number of rows where x is the given number,
+loops through each row giving it x number of divs before appending
+all the rows to the container div */
 
 function createPad () {
     lengthOfPad = document.getElementById("lengthOfGrid").value;
@@ -73,4 +139,18 @@ function createPad () {
         }
         container.appendChild(row);
     }
+}
+
+function changeColor() {
+    defaultColor = 0;
+    rValue = parseInt(document.querySelector("#rValue").value);
+    gValue = parseInt(document.querySelector("#gValue").value);
+    bValue = parseInt(document.querySelector("#bValue").value);
+
+    /* document
+    let coloredState = document.querySelector(".playing");
+    rValue = parseInt(document.querySelector("#rValue").value);
+    gValue = parseInt(document.querySelector("#gValue").value);
+    bValue = parseInt(document.querySelector("#bValue").value);
+    coloredState.style.background = rgb(rValue, gValue, bValue);   */ 
 }
